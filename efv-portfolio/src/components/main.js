c8f7d3d62d05401be2a11e249e87a254f8e5b834
Eric Vondunn <ericvondunn40@gmail.com>
2021-01-16 17:47:30 -0500

    router dom

--- efv-portfolio/src/components/main.js
+++ efv-portfolio/src/components/main.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Landing from "./landingpage";
+import Landing from "./landingPage";
+import About from "./aboutMe";
+import Contact from "./contact";
+import Projects from "./projects";
+import Resume from "./resume";
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={Landing} />
+    <Route path="/aboutMe" component={About} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} />
   </Switch>
 );
 
