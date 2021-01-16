a40658013ac1789d07abeaf64393b742ea0ccad2
Eric Vondunn <ericvondunn40@gmail.com>
2021-01-16 17:38:43 -0500

    components built

--- efv-portfolio/src/App.js
+++ efv-portfolio/src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
-import { render } from "react-dom";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/main";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        {/* Uses a header that scrolls with the text, rather than staying locked at the top */}
+        <div className="demo-big-content">
+          <Layout>
+            <Header title="Title" scroll>
+              <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+              </Navigation>
+            </Header>
+            <Drawer title="Title">
+              <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+              </Navigation>
+            </Drawer>
+            <Content>
+              <div className="page-content" />
+              <Main />
+            </Content>
+          </Layout>
+        </div>
       </div>
     );
   }
